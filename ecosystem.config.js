c22e36bf360867b2fc6312e0dcd63bcbe31e8b3e
w4nnnn <irwanfebriansyah112@gmail.com>
2026-06-27 21:20:55 +0700

    feat: update production port to 2200 in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       args: 'start',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 2200,
       },
       env_development: {
         NODE_ENV: 'development',
